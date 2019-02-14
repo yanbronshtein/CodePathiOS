b215616bd4740742d11da9824ba50b01c7f95dbd
Yaniv Bronshtein <yanbronshtein@gmail.com>
2019-02-13 19:52:24 -0500

    Hello I have committed a hello!

---
+++ Assignment1/Assignment1/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Assignment1
+//
+//  Created by Yaniv Bronshtein on 2/13/19.
+//  Copyright © 2019 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Hello!!!!");
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
