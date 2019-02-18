b3b7ca4ba16a738c510f65ce5b564f75ef557a49
Yaniv Bronshtein <yanbronshtein@gmail.com>
2019-02-18 13:52:31 -0500

    Up to Part 3

---
+++ flixster/flixster/MoviesViewController.swift
@@ -0,0 +1,55 @@
+//
+//  MoviesViewController.swift
+//  flixster
+//
+//  Created by Yaniv Bronshtein on 2/18/19.
+//  Copyright © 2019 codepath. All rights reserved.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController {
+
+    var movies = [[String:Any]]()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        print("Hello")
+        
+        // From assignment page
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            // This will run when the network request returns
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                self.movies = dataDictionary["results"] as! [[String:Any]]
+                
+                print(dataDictionary)
+                // TODO: Get the array of movies
+                // TODO: Store the movies in a property to use elsewhere
+                // TODO: Reload your table view data
+                
+            }
+        }
+        task.resume()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
